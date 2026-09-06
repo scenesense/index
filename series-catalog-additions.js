3c81a867e2f5c70d21a42510ec5bc874da805508
soap2 <46655904+soap2@users.noreply.github.com>
2026-09-06 17:53:13 +0200

    Load One Big Happy catalogue addition

--- series-catalog-additions.js
+++ series-catalog-additions.js
@@ -8,7 +8,8 @@
       `data/series/index-additions-nautilus.json?v=${Date.now()}`,
       `data/series/index-additions-northern-exposure.json?v=${Date.now()}`,
       `data/series/index-additions-mr-sunshine.json?v=${Date.now()}`,
-      `data/series/index-additions-one-tree-hill.json?v=${Date.now()}`
+      `data/series/index-additions-one-tree-hill.json?v=${Date.now()}`,
+      `data/series/index-additions-one-big-happy.json?v=${Date.now()}`
     ];
     const additions=[];
     for(const source of sources){
